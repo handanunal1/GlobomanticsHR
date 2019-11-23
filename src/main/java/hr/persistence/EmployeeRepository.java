befaa06fa163e5d1fc0d88a3fa95e9956ada34a9
handanunal <handan.unal@yahoo.com>
2019-11-23 16:31:47 +0300

    Globomantics - Refactoring logging,save,serializing

--- src/main/java/hr/persistence/EmployeeRepository.java
+++ src/main/java/hr/persistence/EmployeeRepository.java
@@ -4,6 +4,10 @@ import hr.personnel.Employee;
 import hr.personnel.FullTimeEmployee;
 import hr.personnel.PartTimeEmployee;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,15 +19,31 @@ we are storing employees in the file system.
 
 public class EmployeeRepository {
 
-    public List<Employee> findAll(){
+	private EmployeeFileSerializer serializer;
 
-        // Employees are kept in memory for simplicity
-        Employee anna = new FullTimeEmployee("Anna Smith", 2000);
-        Employee billy = new FullTimeEmployee("Billy Leech", 920);
+	public EmployeeRepository() {
+		this.serializer = serializer;
+	}
 
-        Employee steve = new PartTimeEmployee("Steve Jones", 800);
-        Employee magda = new PartTimeEmployee("Magda Iovan", 920);
+	public List<Employee> findAll() {
 
-        return Arrays.asList(anna, billy, steve, magda);
-    }
+		// Employees are kept in memory for simplicity
+		Employee anna = new FullTimeEmployee("Anna Smith", 2000);
+		Employee billy = new FullTimeEmployee("Billy Leech", 920);
+
+		Employee steve = new PartTimeEmployee("Steve Jones", 800);
+		Employee magda = new PartTimeEmployee("Magda Iovan", 920);
+
+		return Arrays.asList(anna, billy, steve, magda);
+	}
+
+	public void save(Employee employee) throws IOException {
+
+		String sealizedString = this.serializer.serialize(employee);
+
+		Path path = Paths.get(employee.getFullName().replace(" ", "_") + ".rec");
+		Files.write(path, sealizedString.toString().getBytes());
+
+	}
+
 }
